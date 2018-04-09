7ff0fc3a6ba4fb0396771370c98945c736ef6fbb
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-09 16:10:58 -0400

    update test data

--- data.py
+++ data.py
@@ -24,11 +24,11 @@ DATA = {
                 "magenta"      : "165",
                 },
             "color" : {
-                "yearly"    : "green red",
-                "monthly"   : "green red",
-                "weekly"    : "green orange",
-                "daily"     : "green blue",
-                "no-repeat" : "green pink",
+                "yearly"    : "green red orange", # default, todo, late
+                "monthly"   : "green red yellow",
+                "weekly"    : "green orange red",
+                "daily"     : "green blue red",
+                "no-repeat" : "green pink magenta",
                 },
             "dimension" : {
                 "cell-width" : "16",
@@ -42,7 +42,8 @@ DATA = {
                     {"name" : "Test Year 1" , "month" : "4"  , "day" : "1"  , "whole-day" : "no"  , "hour" : "10" , "length" : "120"},
                     ],
                 "todo" : [
-                    {"name" : "Test Year 2", "month" : "4", "day" : "15", "whole-day" : "yes", "hour" : "", "length" : ""},
+                    {"name" : "Test Year 2", "month" : "4", "day" : "15", "whole-day" : "yes",
+                                            "hour" : "", "length" : "", "done" : "no"},
                     ],
                 },
             "monthly" : {
@@ -51,11 +52,25 @@ DATA = {
                     {"name" : "Test Month 2" , "day" : "20" , "whole-day" : "no"  , "hour" : "14" , "length" : "30"},
                     ],
                 "todo" : [
-                    {"name" : "Test Month 3", "day" : "15", "whole-day" : "yes", "hour" : "", "length" : ""},
+                    {"name" : "Test Month 3", "day" : "15", "whole-day" : "yes", "hour" : "", "length" : "", "done" : "no"},
+                    {"name" : "Test Month 4", "day" : "15", "whole-day" : "yes", "hour" : "", "length" : "", "done" : "yes"},
                     ],
                 },
             "weekly" : {
                 "default" : [
+                    {"name" : "259 Lecture"           , "day" : "1" , "hour" : "9 30"  , "length" : "50"},
+                    {"name" : "Interp Session"        , "day" : "1" , "hour" : "12 30" , "length" : "50"},
+                    {"name" : "StuCo Origami"         , "day" : "1" , "hour" : "18 30" , "length" : "80"},
+                    {"name" : "259 Recitation"        , "day" : "2" , "hour" : "10 30" , "length" : "50"},
+                    {"name" : "150 Lecture"           , "day" : "2" , "hour" : "12"    , "length" : "80"},
+                    {"name" : "150 Lab"               , "day" : "3" , "hour" : "10 30" , "length" : "80"},
+                    {"name" : "251 Homework Writing"  , "day" : "3" , "hour" : "18 30" , "length" : "80"},
+                    {"name" : "251 Lecture"           , "day" : "4" , "hour" : "15"    , "length" : "80"},
+                    {"name" : "Interp Session"        , "day" : "5" , "hour" : "12 30" , "length" : "80"},
+                    {"name" : "251 Rewrite"           , "day" : "5" , "hour" : "16 30" , "length" : "50"},
+                    {"name" : "251 Homework Solution" , "day" : "6" , "hour" : "12 30" , "length" : "60"},
+                    {"name" : "251 Homework Solution" , "day" : "0" , "hour" : "12 30" , "length" : "60"},
+                    {"name" : "251 Recitation"        , "day" : "6" , "hour" : "14 30" , "length" : "60"},
                     ],
                 "todo" : [
                     ],
