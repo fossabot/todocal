dddbd843bb24d78a8c48ddd9e7d05ac5c6802f55
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-09 15:24:56 -0400

    initial commit data structure

---
+++ data.py
@@ -0,0 +1,67 @@
+# TodoCal Data Structure in Python
+
+DATA = {
+        "meta" : {
+            "color-code" : {
+                "white" : "",
+                "black" : "",
+                "red" : "",
+                "blue" : "",
+                "light-blue" : "",
+                "dark-blue" : "",
+                "green" : "",
+                "light-green" : "",
+                "dark-green" : "",
+                "yellow" : "",
+                "pink" : "",
+                "orange" : "",
+                "purple" : "",
+                "light-purple" : "",
+                "dark-purple" : "",
+                "brown" : "",
+                "teal" : "",
+                "cyan" : "",
+                "magenta" : "",
+                },
+            "color" : {
+                "yearly" : "green red",
+                "monthly" : "green red",
+                "weekly" : "green orange",
+                "daily" : "green blue",
+                "no-repeat" : "green pink",
+                },
+            },
+        "events" : {
+            "yearly" : {
+                "default" : {
+                    },
+                "todo" : {
+                    },
+                },
+            "monthly" : {
+                "default" : {
+                    },
+                "todo" : {
+                    },
+                },
+            "weekly" : {
+                "default" : {
+                    },
+                "todo" : {
+                    },
+                },
+            "daily" : {
+                "default" : {
+                    },
+                "todo" : {
+                    },
+                },
+            "no-repeat" : {
+                "default" : {
+                    },
+                "todo" : {
+                    },
+                },
+            },
+        }
+
