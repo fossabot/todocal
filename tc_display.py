db41e40329b91e701c33001142f710c31d4c8346
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-13 15:14:09 -0400

    display handle event start end time code

--- tc_display.py
+++ tc_display.py
@@ -4,6 +4,7 @@
 
 from tc_print import pass_info
 import tc_meta
+import datetime
 
 def __get_dimension_info (raw_info):
     try:
@@ -31,3 +32,32 @@ def __get_dimension_info (raw_info):
 
     return (calendar_width, max_height, (time_col_width, between_col_width, col_width))
 
+def __get_calendar_height (max_height, col_width, E):
+    # information in e in E
+    # name, month, day, hour, length, (done), mark (0/1 char)
+
+    for e in E:
+        if ' ' not in e ["hour"]:
+            e ["hour"] += " 00"
+    for e in E:
+        e ["time-start-code"] = int (e ["hour"].replace (' ', ''))
+        try:
+            start_time = [int (s) for s in e ["hour"].split (' ')]
+            start_hour = start_time [0]
+            start_minute = start_time [1]
+        except:
+            tc_meta.raise_ERROR ("")
+        start_time_object = datetime.time (hour = start_hour, minute = start_minute)
+        try:
+            if e ["length"] == "":
+                end_time_object = start_time_object
+            else:
+                end_time_object = start_time_object + datetime.timedelta (minutes = int (e ["length"]))
+        except:
+            tc_meta.raise_ERROR ("")
+        time_end_code = end_time_object.hour * 100 + end_time_object.minute
+        if time_end_code < e ["time-start-code"]:
+            time_end_code = 2359
+        e ["time-end-code"] = time_end_code
+    # e.g. hour = "15 20", length = "30"
+    # time-start-code = 1520, time-end-code = 1550
