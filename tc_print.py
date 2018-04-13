16b0643c45a07be7a18b3c51a3655c064c72ff7c
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-12 21:24:09 -0400

    prints

--- tc_print.py
+++ tc_print.py
@@ -8,3 +8,28 @@ def print_week_defaults ():
     TIME_INFO = tc_time.get_time_info ()
     DATA = tc_data.DATA
     summary = tc_handler.summarize_defaults (DATA, TIME_INFO)
+
+    for e in summary:
+        print (u'\u001b[38;5;' + e ["color"] + 'm' +
+                e ["month"] + ' ' +
+                e ["day"] + ' ' +
+                e ["hour"].replace (' ', ':') + ' ' +
+                e ["name"] +
+                u'\u001b[0m')
+
+    return 0
+
+def print_week_todos ():
+    TIME_INFO = tc_time.get_time_info ()
+    DATA = tc_data.DATA
+    summary = tc_handler.summarize_todos (DATA, TIME_INFO)
+
+    for e in summary:
+        print (u'\u001b[38;5;' + e ["color"] + 'm' +
+                e ["month"] + ' ' +
+                e ["day"] + ' ' +
+                e ["hour"].replace (' ', ':') + ' ' +
+                e ["name"] +
+                u'\u001b[0m')
+
+    return 0
