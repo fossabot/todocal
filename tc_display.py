e2cb3d422b175ac9472d54530e52035c15a8489e
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-13 14:48:11 -0400

    display handle dimensions info

--- tc_display.py
+++ tc_display.py
@@ -13,3 +13,21 @@ def __get_dimension_info (raw_info):
             cell_width = int (raw_info ["max-width"]) // 8
         except:
             tc_meta.raise_ERROR ("Error: calendar dimensions not well defined.")
+
+    if cell_width <= 7:
+        tc_meta.raise_ERROR ("TodoCal: unimplemented situation.")
+    # assert (cell_width >= 8)
+
+    time_col_width = 7 # allow full time display
+    between_col_width = 1
+    col_width = cell_width
+
+    calendar_width = time_col_width + col_width * 7 + between_col_width * 7
+
+    try:
+        max_height = int (raw_info ["max-height"])
+    except:
+        tc_meta.raise_ERROR ("Error: calendar dimensions not well defined.")
+
+    return (calendar_width, max_height, (time_col_width, between_col_width, col_width))
+
