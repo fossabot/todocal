f70363b459126662ae9725bf7e49e1a4f7909c2d
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-13 14:09:45 -0400

    program meta control error handling

---
+++ tc_meta.py
@@ -0,0 +1,17 @@
+# TodoCal Meta Program Control
+# ----------------------------
+# Catelog
+# raise_ERROR (msg): raise fatal error and quit program
+# raise_WARNING (msg): print warning message continue program
+
+def raise_ERROR (msg):
+    if msg == "":
+        msg = "TodoCal Error."
+    print ('\u001b[31;1m' + msg + '\u001b[0m')
+    quit()
+
+def raise_WARNING (msg):
+    if msg == "":
+        msg = "TodoCal Warning."
+    print ('\u001b[33;1m' + msg + '\u001b[0m')
+
