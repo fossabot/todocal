efb2968478d9ca3b46c64cce9e8a352452f59403
Sam Yong <yongmc.sam2014@outlook.com>
2018-04-13 15:27:27 -0400

    display handle event time spans

--- tc_display.py
+++ tc_display.py
@@ -58,6 +58,22 @@ def __get_calendar_height (max_height, col_width, E):
         time_end_code = end_time_object.hour * 100 + end_time_object.minute
         if time_end_code < e ["time-start-code"]:
             time_end_code = 2359
+        if time_end_code != 0 and time_end_code % 100 == 0:
+            time_end_code -= 41 # reduce to past hour
         e ["time-end-code"] = time_end_code
     # e.g. hour = "15 20", length = "30"
     # time-start-code = 1520, time-end-code = 1550
+
+    span_mark = []
+    for _ in range(24):
+        span_mark.append (0)
+        # span_mark [i] => time from i hour to (i + 1) hour
+    for e in E:
+        for i in range (e ["time-start-code"] // 100, e ["time-end-code"] // 100 + 1):
+            span_mark [i] = 1
+    spans = []
+    for i in range(24):
+        if span_mark [i] == 1:
+            spans.append (i)
+    # e.g. spans = [0, 7, 8, 10, 15]
+    # => exist events in 0-1, 7-9, 10-11, 15-16
